db99c32c55128ac13fe9d65890025f1454564c57
Erkin Aydın <erkinresearch@gmail.com>
2023-10-14 21:07:20 +0300

    no shit changed.

--- hw1/part2_c.py
+++ hw1/part2_c.py
@@ -2,9 +2,9 @@ import numpy as np
 joint_angles= [1.95205226, 3.15753276, 0]
 
 def Jacobian(angles):
-    #return np.array([[(np.sin(np.pi/2-angles[0]) + np.sin(np.pi/2-angles[0]-angles[1])), np.sin(np.pi/2-angles[0]-angles[1])], 
-    #                [(-np.cos(np.pi/2-angles[0]) - np.cos(np.pi/2-angles[0]-angles[1])), - np.cos(np.pi/2-angles[0]-angles[1])]])
-      return np.array([[(np.cos( angles[0]) + np.cos( angles[0] + angles[1])), np.cos( angles[0] +angles[1])], 
-                     [-(np.sin( angles[0]) + np.sin( angles[0] + angles[1])), -np.sin( angles[0] + angles[1])]])
+    return np.array([[(np.sin(np.pi/2-angles[0]) + np.sin(np.pi/2-angles[0]-angles[1])), np.sin(np.pi/2-angles[0]-angles[1])], 
+                    [(-np.cos(np.pi/2-angles[0]) - np.cos(np.pi/2-angles[0]-angles[1])), - np.cos(np.pi/2-angles[0]-angles[1])]])
+    #  return np.array([[(np.cos( angles[0]) + np.cos( angles[0] + angles[1])), np.cos( angles[0] +angles[1])], 
+    #                 [-(np.sin( angles[0]) + np.sin( angles[0] + angles[1])), -np.sin( angles[0] + angles[1])]])
 
 print(Jacobian(joint_angles))
