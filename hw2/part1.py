4a464281ce39da2dc7169701227a0d19967985f5
Arda YILDIZ <83536771+ArdaYildiz1@users.noreply.github.com>
2023-11-07 22:07:00 +0300

    chore: add part 1 file
    RRT partially works. We get closer to the destination but the simulation stops when we collide the wall. Normally it should not collide but we could not find the reason.

---
+++ hw2/part1.py
@@ -0,0 +1,51 @@
+from robotic import ry
+import numpy as np
+import time
+
+C = ry.Config()
+C.addFile('Gfiles/cargobot_base.g')
+C.addFile('Gfiles/maze.g')
+
+qHome = C.getJointState()
+C.setJointState(qHome)
+komo = ry.KOMO(C, 1, 10, 2, True)
+
+komo.addControlObjective([], 1, 1e-1)
+komo.addControlObjective([], 2, 1e0)
+
+komo.addObjective([], ry.FS.accumulatedCollisions, [], ry.OT.eq,[1e2])
+komo.addObjective([1], ry.FS.positionDiff, ['base', 'goal_area'], ry.OT.eq, [1e1])
+komo.addObjective([1], ry.FS.jointState, [], ry.OT.eq, [1e1], [], order=1)
+
+ret = ry.NLP_Solver(komo.nlp(), verbose=0 ) .solve()
+print(ret)
+q = komo.getPath()
+
+print('size of path:', q.shape)
+
+for t in range(q.shape[0]):
+    C.setJointState(q[t])
+    C.view(False, f'waypoint {t}')
+    time.sleep(.1)
+
+# that's the goal configuration
+qT = komo.getPath()[-1]
+C.setJointState(qT)
+C.view(False, "IK solution")
+
+#define a path finding problem
+rrt = ry.PathFinder()
+rrt.setProblem(C, [qHome], [qT])
+
+ret = rrt.solve()
+print(ret)
+path = ret.x
+
+# display the path
+for t in range(0, path.shape[0]-1):
+    C.setJointState(path[t])
+    C.view()
+    time.sleep(.1)
+
+C.view()
+
