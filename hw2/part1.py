fa18d268c521010267b880d943ace6fbecb500a9
Arda YILDIZ <83536771+ArdaYildiz1@users.noreply.github.com>
2023-11-08 15:00:04 +0300

    update part1.py

--- hw2/part1.py
+++ hw2/part1.py
@@ -41,6 +41,39 @@ ret = rrt.solve()
 print(ret)
 path = ret.x
 
+komo2 = ry.KOMO(C, 1, 10, 2, True)
+komo2.addControlObjective([], 1, 1e-1)
+komo2.addControlObjective([], 2, 1e0)
+
+komo2.addObjective([], ry.FS.accumulatedCollisions, [], ry.OT.eq,[1e2])
+komo2.addObjective([1], ry.FS.positionDiff, ['base', 'goal_area'], ry.OT.eq, [1e1])
+komo2.addObjective([1], ry.FS.jointState, [], ry.OT.eq, [1e1], [], order=1)
+
+ret = ry.NLP_Solver(komo2.nlp(), verbose=0 ) .solve()
+print(ret)
+q = komo2.getPath()
+
+print('size of path:', q.shape)
+
+for t in range(q.shape[0]):
+    C.setJointState(q[t])
+    C.view(False, f'waypoint {t}')
+    time.sleep(.1)
+
+# that's the goal configuration
+qT = komo2.getPath()[-1]
+C.setJointState(qT)
+
+#define a path finding problem
+rrt = ry.PathFinder()
+rrt.setProblem(C, [qHome], [qT])
+
+ret = rrt.solve()
+print(ret)
+path = ret.x
+
+komo2.initWithPath_qOrg(path)
+
 # display the path
 for t in range(0, path.shape[0]-1):
     C.setJointState(path[t])
